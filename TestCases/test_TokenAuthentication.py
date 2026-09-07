c8a0ec87e1339be8451aafc607461d111de36043
qamastery <kostyantyn.morozovskyy@gmail.com>
2026-09-07 10:05:47 +0300

    Removed secret from tokenaythentication testcase

--- TestCases/test_TokenAuthentication.py
+++ TestCases/test_TokenAuthentication.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_token_auth():
-    access_token = "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"
+    access_token = ""
     api_url = "https://api.github.com/user"
     headers = {"Authorization": f"Bearer {access_token}",
                "X-GitHub-Api-Version": "2026-03-10"
